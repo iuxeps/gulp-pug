7a5a53b2456848602a764ad065463e12c4142e13
islom-karimov <islom.kdev@gmail.com>
2023-01-10 11:03:53 +0500

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+// import { gsap } from 'gsap'
+// import Swiper, { Navigation, Pagination, EffectCreative, EffectFade, Thumbs, FreeMode } from 'swiper';
+// import { ScrollTrigger } from 'gsap/dist/ScrollTrigger.js'
+
